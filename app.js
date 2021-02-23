8839e449367d38320af92b59fe0b12c54faa7ceb
nakullondhe <65081281+nakullondhe@users.noreply.github.com>
2021-02-23 10:56:41 +0530

    add backend files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const serverless = require("serverless-http");
+var router = require("express").Router();
 const app = express();
 
 // DB connection
@@ -25,6 +27,13 @@ app.use(cookieParser());
 app.use(cors());
 
 //Register Routes
+router.get(`/`, (req, res)=> {
+  res.json({
+    message: "bpoom bam"
+  })
+})
+
+app.use('/.netlify/functions/app', router);
 app.use("/api", require('./routes/Auth'));
 app.use("/api", require('./routes/Match'));
 app.use("/api", require('./routes/User'));
@@ -38,3 +47,6 @@ app.listen(port , () => {
   console.log(`app is running at port ${port}`)
 });
 
+module.exports = app;
+
+module.exports.handler = serverless(app);
--- routes/User.js
+++ routes/User.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 // const { Signup } = require('../controllers/Auth');
 // const User = require("../models/user");
 
-router.get('/', (req, res) => {
+router.get('/test', (req, res) => {
   res.send({
     message: "You hit the api"
   })
