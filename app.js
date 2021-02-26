2fcc92b08b87d959d89391f43a31d1473d6b5a3d
nakullondhe <65081281+nakullondhe@users.noreply.github.com>
2021-02-26 20:00:13 +0530

    TImer and ui changes left

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.DATABASE1,  {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
