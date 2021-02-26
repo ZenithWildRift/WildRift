de5691459d699ec45b1e89ab1a28b99ea6c6d792
nakullondhe <65081281+nakullondhe@users.noreply.github.com>
2021-02-26 22:09:37 +0530

    Boom

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ mongoose.connect(process.env.DATABASE1,  {
   console.log("DB CONNECTED");
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 const io = require("./websocket/index");
 
@@ -48,7 +47,7 @@ app.use((err, req, res, next) => {
 })
 
 // PORT
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 // START SERVER
 server.listen(port , () => {
