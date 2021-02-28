422f081d2ae0dc9874ef70f01d9dc6b9afc21ca2
nakullondhe <65081281+nakullondhe@users.noreply.github.com>
2021-02-28 16:51:56 +0530

    added new boilerplate

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ mongoose.connect(process.env.DATABASE1,  {
   console.log("DB CONNECTED");
 });
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const io = require("./websocket/index");
 
 // MIDDLEWARES
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
